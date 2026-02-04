8316378e16b6f34f490d38823c986f4e1f0dcc6d
Jerome Isaac Cereneo <mumuchim.jic@gmail.com>
2026-02-04 18:13:36 +0800

    day 7

---
+++ 35_array_iteration.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+char array;
+
+int main()
+{
+    std::string manaloto[] = {"Pepito", "Elsa", "Chito", "Clarissa"};
+
+    for (int i = 0; i < sizeof(manaloto) / sizeof(std::string); i++)
+    // with sizeof()/size(type) you can make automatic array displayer wow
+    {
+        std::cout << manaloto[i] << '\n';
+    }
+
+    char rarity[] = {'M', 'L', 'E', 'R', 'C'};
+
+    for (int i = 0; i < sizeof(rarity) / sizeof(char); i++)
+
+    {
+        std::cout << rarity[i] << '\n';
+    }
+
+    return 0;
+}
