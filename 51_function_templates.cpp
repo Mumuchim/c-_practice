c4e66f1b56aeb423b92088df5a0ee0b279ea5f37
Jerome Isaac Cereneo <mumuchim.jic@gmail.com>
2026-02-07 13:22:34 +0800

    day 11

--- 51_function_templates.cpp
+++ 51_function_templates.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 template <typename T> // this is like auto but it is better to use in functions with parameters
                       // it's like a class but only for data types i think
-T max(T x, T y)
+
+T max(T x, T y) // only set to receive arguments of the same data type
 {
     return (x > y) ? x : y;
 }
+
+template <typename J, typename K> // you can declare different templates if u want  different data types
+
+auto max(J x, K y) // then use auto incase of return
+{
+    return (x > y) ? x : y;
+}
+
 int main()
 {
     std::cout << max(1, 2) << '\n';
     std::cout << max(1.1, 2.2) << '\n';
     std::cout << max('1', '2') << '\n';
-
+    std::cout << max(7, 9.11) << '\n'; // now you can return different data types with one template
     return 0;
 }
